762da693613e66a21e9e8cf2a9d17504f4433a66
Justin Zhang <oszcoszc@gmail.com>
2014-07-21 16:47:36 +0800

    commit

--- my_pwd2.c
+++ my_pwd2.c
@@ -1,3 +1,32 @@
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/types.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+/***
+1.if has parent
+     go to parent
+  print current folder name
+*/
 
+int main(int argc,char* argv[]){
+
+    
+
+
+
+}
+
+int getNodeId(char *fileName){
+
+   int fd = open(fileName,O_RDONLY);
+    
+    if(fd == -1){
+        perror("can not open file");
+        exit(EXIT_REASON_IO_INSTRUCTION);
+    }
+
+    
+
+
+}
