bce9b32b8890aba80acb3761db6dd20c81ae300d
Justin Zhang <oszcoszc@gmail.com>
2014-07-27 13:58:58 +0800

    mmm

---
+++ hello1.c
@@ -0,0 +1,23 @@
+//
+//  main.c
+//  MyCursesGame
+//
+//  Created by JustinZhang on 7/25/14.
+//  Copyright (c) 2014 Justin. All rights reserved.
+//
+
+#include <stdio.h>
+#include<curses.h>
+int main(int argc, const char * argv[])
+{
+
+    initscr();
+    clear();
+    move(10, 20);
+    addstr("Hello World");
+    move(LINES-1, 0);
+    refresh();
+    getch();
+    endwin();
+    return 0;
+}
